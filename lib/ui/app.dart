2f2529ac3beb82411f9e4721f3c5fe7bd7db9d0d
Dario12311 <dariomolinarua15@gmail.com>
2025-10-08 17:48:28 -0500

    [COMMIT:  Configuracion de carpetas]

---
+++ lib/ui/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Hibernadero',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: const Scaffold(
+        body: Center(child: Text('Hola Hibernadero')),
+      ),
+    );
+  }
+}
